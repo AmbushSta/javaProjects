1388e36d79346bca265a266f3e12f59f5ba099a0
Michael Cowie <mco116@uclive.ac.nz>
2017-01-17 01:56:37 +1300

    Create 21UsingRecursion.java

---
+++ 21UsingRecursion.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+/**
+ * Takes a sequence of numbers from the user as input such as "1 9 11 5 6", and prints to combine
+ * all combinations of the numbers that add to 21:
+ * Example input: 1 9 11 5 6
+ * Successfully outputs:
+ * The values 1 9 11 Add up to 21
+ * The values 1 9 5 6 Add up to 21
+ */
+public class TwentyOne {
+
+   public static void main(String args[]){
+      Scanner userInput = new Scanner(System.in);
+      String stringInput = userInput.nextLine();
+
+      ArrayList<Integer> allNum = new ArrayList<>();
+      for (String num : stringInput.split(" ")){
+         allNum.add(Integer.parseInt(num));
+      }
+
+      HashSet<ArrayList<Integer>> answer = twentyOne(allNum);
+
+      if(answer.size() == 0){
+         System.out.print("No possible number combinations equal 21");
+         return;
+      }
+
+      for (ArrayList<Integer> solution : answer){
+         System.out.print("The values ");
+         for (Integer num : solution){
+            System.out.print(num + " ");
+         }
+         System.out.println("Add up to 21");
+      }
+   }
+
+
+
+   private static HashSet twentyOne(ArrayList<Integer> userInput){
+      ArrayList<Integer> stack = new ArrayList<>();
+      HashSet<ArrayList<Integer>> answer = new HashSet<>();
+      return twentyOneRecursive(userInput, stack, answer);
+   }
+
+   private static HashSet<ArrayList<Integer>> twentyOneRecursive(ArrayList<Integer> userInput,ArrayList<Integer> stack, HashSet<ArrayList<Integer>> answer){
+      //Equivilent to enumerate in python (To know index of which position of a character from the list im iterating over)
+      ListIterator<Integer> userInputIterator = userInput.listIterator();
+      while (userInputIterator.hasNext()){
+         //Creating a new_stack object because you cannot append
+         ArrayList<Integer> new_stack = new ArrayList<>(stack);
+         new_stack.add(userInputIterator.next());
+
+         int sum = sum(new_stack);
+
+         if (sum == 21){
+            answer.add(new_stack);
+         } else if (sum < 21){
+            twentyOneRecursive(new ArrayList<>(userInput.subList(userInputIterator.nextIndex(), userInput.size())), new_stack, answer);
+         }
+      }
+      return answer;
+   }
+
+   private static int sum(ArrayList<Integer> nums){
+      int sum = 0;
+      for (int num : nums){
+         sum += num;
+      }
+      return sum;
+   }
+}
