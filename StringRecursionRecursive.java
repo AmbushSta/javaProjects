31429fc787803f681ef22759e706715078fb6201
Michael Cowie <mco116@uclive.ac.nz>
2017-02-06 00:24:28 +1300

    Update StringRecursionRecursive.java

--- StringRecursionRecursive.java
+++ StringRecursionRecursive.java
@@ -3,6 +3,29 @@
  */
 public class StringReversalRecursion {
 
+   public static void main(String[] args){
+      StringReversalRecursion answer = new StringReversalRecursion();
+      System.out.println("Result: " + answer.stringRecursion("test123"));
+   }
+
+   String reversedStr = "";
+
+   private String stringRecursion(String str){
+      if(str.length() == 1){
+         return str;
+      }else{
+         reversedStr += str.charAt(str.length() -1) + stringRecursion(str.substring(0, str.length() - 1));
+         return reversedStr;
+      }
+   }
+}
+
+/**
+ * Another implementation
+*/
+
+public class StringReversalRecursion {
+
 
    public static void main(String[] args){
       StringReversalRecursion answer = new StringReversalRecursion();
