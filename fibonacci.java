4378d477ece576df266b97db0c5f687948bdc39c
Michael Cowie <mco116@uclive.ac.nz>
2017-02-08 04:57:15 +1300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,22 @@
+import java.util.HashMap;
+
 /**
- * Returns nth fibonacci number
+ * Returns nth fibonacci number with pruning added for efficiency
  */
 public class fib {
+   static HashMap<Integer, Integer> fibHash = new HashMap<>();
 
    public static void main(String[] args){
-      //Example
+      fibHash.put(0,0);
+      fibHash.put(1,1);
       System.out.println(fibonacci(12));
    }
 
    public static int fibonacci(int n) {
-      if (n == 0){
-         return 0;
-      } else if (n == 1){
-         return 1;
-      } else {
-         return fibonacci(n - 1) + fibonacci(n - 2);
-      }
+      int x = fibHash.containsKey(n - 1) ? fibHash.get(n - 1) : fibonacci(n - 1);
+      fibHash.put(n - 1, x);
+      int y = fibHash.containsKey(n - 2) ? fibHash.get(n - 2) : fibonacci(n - 2);
+      fibHash.put(n - 2, y);
+      return x + y;
    }
 }
